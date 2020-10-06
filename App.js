254a873f13749342c28ea20e9b450b8f5286b2d3
muhammadzee343 <muhammad.zee343@gmail.com>
2020-10-06 09:48:47 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Running</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
